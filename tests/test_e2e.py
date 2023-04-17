936c90d7a374d5e15ff4c12cdbbb6a48741417d5
Aleksander Wojnarowicz <alwojnarowicz@gmail.com>
2023-04-17 18:16:43 +0200

    test logging context parts

--- tests/test_e2e.py
+++ tests/test_e2e.py
@@ -6,31 +6,41 @@ from datetime import (
 from hashlib import md5
 
 import neptune
+import pytest
 from airflow import DAG
 from airflow.decorators import task
 
 from neptune_airflow import get_run_from_context
 
 
-def test_e2e():
-    with DAG(
-        dag_id="test_dag",
-        description="test_description",
-        schedule="@daily",
-        start_date=datetime.today() - timedelta(days=1),
-    ) as dag:
+class TestE2E:
+    @pytest.mark.parametrize("log_context", [True, False])
+    def test_metrics_and_context_logged(self, log_context):
+        with DAG(
+            dag_id="test_dag",
+            description="test_description",
+            schedule="@daily",
+            start_date=datetime.today() - timedelta(days=1),
+        ) as dag:
 
-        @task(task_id="hello")
-        def task1(**context):
-            neptune_run = get_run_from_context(context=context)
-            neptune_run["some_metric"] = 5
-            os.environ["NEPTUNE_CUSTOM_RUN_ID"] = md5(context["dag_run"].run_id.encode()).hexdigest()
-            neptune_run.sync()
-            neptune_run.stop()
+            @task(task_id="hello")
+            def task1(**context):
+                neptune_run = get_run_from_context(context=context, log_context=log_context)
+                neptune_run["some_metric"] = 5
+                os.environ["NEPTUNE_CUSTOM_RUN_ID"] = md5(context["dag_run"].run_id.encode()).hexdigest()
+                neptune_run.sync()
+                neptune_run.stop()
 
-        task1()
+            task1()
 
-        dag.test()
+            dag.test()
 
-    run = neptune.init_run()
-    assert run["some_metric"].fetch() == 5
+        run = neptune.init_run()
+        assert run["some_metric"].fetch() == 5
+        assert run.exists("source_code/integrations/airflow")
+
+        if log_context:
+            assert run.exists("context")
+            assert run.exists("context/dag")
+            assert run["context/dag/_dag_id"].fetch() == "test_dag"
+            assert run["context/dag/_description"].fetch() == "test_description"
