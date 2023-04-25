f6422bc78a6956e4aa426cf83f9e917004cc4111
Aleksander Wojnarowicz <alwojnarowicz@gmail.com>
2023-04-25 09:41:24 +0200

    log context in seperate function + add docstrings

--- src/neptune_airflow/impl/__init__.py
+++ src/neptune_airflow/impl/__init__.py
@@ -25,6 +25,7 @@ from typing import (
     Any,
     Dict,
     Optional,
+    Union,
 )
 
 from airflow.models import Variable
@@ -49,6 +50,16 @@ from neptune_airflow.impl.version import __version__
 INTEGRATION_VERSION_KEY = "source_code/integrations/airflow"
 
 
+def _log_context(context: Dict[str, Any], neptune_run: Union[Run, Handler]) -> None:
+    for field in {"conf", "dag", "dag_run"}:
+        to_log = context.pop(field, None)
+        if to_log:
+            neptune_run[f"context/{field}"] = stringify_unsupported(to_log.__dict__)
+
+    for key in context:
+        neptune_run[f"context/{key}"] = str(context[key])
+
+
 def get_run_from_context(
     *,
     api_token: Optional[str] = None,
@@ -56,8 +67,33 @@ def get_run_from_context(
     log_context: bool = False,
     context: Dict[str, Any],
 ) -> Run:
-    # Check airflow variables if api_token or project not provided
+    """Creates a Neptune.ai run based on the relevant DAG run.
 
+    This ensures that different tasks will still log to the same run if this function is used.
+    Additionally, context of the task may be logged automatically if `log_context` param is set to True.
+
+    Args:
+        api_token (`str`, optional): API token for Neptune.ai
+        project (`str`, optional): Name of the Neptune.ai project you want to log to.
+        log_context (`bool`): If True, the task context will also be logged to the run. Defaults to False.
+        context (`dict`): The task context obtained from airflow.
+            Note: There are 3 ways of passing api_token and project:
+             - explicitly, passing them as args to the function
+             - by environmental variables (https://docs.neptune.ai/logging/new_run)
+             - by airflow variables (https://airflow.apache.org/docs/apache-airflow/stable/howto/variable.html)
+
+    Returns:
+        `Run` with CUSTOM_RUN_ID based on DAG run ID.
+
+    See the relevant examples in examples/ directory.
+
+
+    For more, see the docs:
+        Tutorial: https://docs.neptune.ai/integrations/airflow/
+        API reference: https://docs.neptune.ai/api/integrations/airflow/
+    """
+
+    # Check airflow variables if api_token or project not provided
     api_token = api_token or Variable.get("NEPTUNE_API_TOKEN", None)
 
     project = project or Variable.get("NEPTUNE_PROJECT", None)
@@ -66,20 +102,14 @@ def get_run_from_context(
     run = init_run(
         api_token=api_token,
         project=project,
-        custom_run_id=md5(dag_run_id.encode()).hexdigest(),
+        custom_run_id=md5(dag_run_id.encode()).hexdigest(),  # CUSTOM_RUN_ID max length = 32
     )
 
     if not run.exists(INTEGRATION_VERSION_KEY):
         run[INTEGRATION_VERSION_KEY] = __version__
 
     if log_context and not run.exists("context"):
-        for field in {"conf", "dag", "dag_run"}:
-            to_log = context.pop(field, None)
-            if to_log:
-                run[f"context/{field}"] = stringify_unsupported(to_log.__dict__)
-
-        for key in context:
-            run[f"context/{key}"] = str(context[key])
+        _log_context(context, run)
 
     return run
 
@@ -91,6 +121,32 @@ def get_task_handler_from_context(
     log_context: bool = False,
     context: Dict[str, Any],
 ) -> Handler:
+    """Creates a Neptune.ai handler based on the relevant DAG run and task ID.
+
+    This ensures that different tasks will still log to the same run if this function is used.
+    Each task will however create its own namespace within the run to log to.
+    Additionally, context of the task may be logged automatically if `log_context` param is set to True.
+
+    Args:
+        api_token (`str`, optional): API token for Neptune.ai
+        project (`str`, optional): Name of the Neptune.ai project you want to log to.
+        log_context (`bool`): If True, the task context will also be logged to the run. Defaults to False.
+        context (`dict`): The task context obtained from airflow.
+            Note: There are 3 ways of passing api_token and project:
+             - explicitly, passing them as args to the function
+             - by environmental variables (https://docs.neptune.ai/logging/new_run)
+             - by airflow variables (https://airflow.apache.org/docs/apache-airflow/stable/howto/variable.html)
+
+    Returns:
+        `Handler` with CUSTOM_RUN_ID based on DAG run ID and namespace based on task ID.
+
+    See the relevant examples in examples/ directory.
+
+
+    For more, see the docs:
+        Tutorial: https://docs.neptune.ai/integrations/airflow/
+        API reference: https://docs.neptune.ai/api/integrations/airflow/
+    """
     base_namespace = context["ti"].task_id
     run = get_run_from_context(api_token=api_token, project=project, log_context=log_context, context=context)
     return run[base_namespace]
